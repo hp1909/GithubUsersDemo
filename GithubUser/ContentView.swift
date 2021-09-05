f3e289212f369620025ead3c2361de7586fe7158
hp1909 <hoangphuc1909@live.com>
2021-09-05 12:44:20 +0700

    Initial Commit

---
+++ GithubUser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubUser
+//
+//  Created by Phuc Hoang on 9/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubUser/GithubUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUserApp.swift
+//  GithubUser
+//
+//  Created by Phuc Hoang on 9/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
